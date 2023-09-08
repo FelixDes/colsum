2c24c52ed881b625212c3c435bd1272cedb6d33c
f.desyatirikov <f.desyatirikov@tinkoff.ru>
2023-09-09 00:10:26 +0300

    ✨Add executable jar ➖Remove logging ♻️Refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
 }
 
-group = "org.example"
+group = "org.colsum"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,8 +11,6 @@ repositories {
 }
 
 dependencies {
-    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-    implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
@@ -28,4 +26,21 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar)
+    }
 }
---
+++ src/main/kotlin/translator/parser/FunctionParser.kt
@@ -0,0 +1,118 @@
+package translator.parser
+
+import translator.nodes.ASTNode
+import translator.nodes.ColorNode
+import translator.nodes.NumberNode
+import translator.tokenization.TokenType
+
+internal sealed class FunctionParser<ResT : ASTNode<*>, ArgT : ASTNode<*>>(
+    tokens: List<Pair<TokenType, String>>
+) : Parser<ResT>(tokens) {
+
+    internal class ArgsParser<FuncArgT : ASTNode<*>>(
+        tokens: List<Pair<TokenType, String>>,
+        private val argParser: Parsable<FuncArgT>,
+        private val separatorList: List<Parsable<*>>
+    ) : Parser<ASTNode.FunctionRepresentationNode<FuncArgT>>(tokens) {
+        override fun consumeDelegate(pos: Int): ParseResult<ASTNode.FunctionRepresentationNode<FuncArgT>> {
+            val functionNameRes = LexemeByTokenParser(tokens, TokenType.FUN_NAME).consume(pos)
+            var localPos = functionNameRes.posOffset
+            val name: String = functionNameRes.nodeList[0].compute()
+
+            val argsResult: ParseResult<FuncArgT> = ParenthesisWrapperParser(
+                tokens, ArgSeparatorListParser(
+                    tokens, argParser, separatorList
+                )
+            ).consume(pos + localPos)
+            localPos += argsResult.posOffset
+
+            val argNodes: List<FuncArgT> = argsResult.nodeList
+
+            return ParseResult(
+                localPos, listOf(
+                    ASTNode.FunctionRepresentationNode(
+                        ASTNode.FunctionRepresentationNode.FunctionRepresentation(
+                            name,
+                            argNodes,
+                            localPos
+                        )
+                    )
+                )
+            )
+        }
+    }
+
+    // READY TESTED
+    class CalcFunctionParser(tokens: List<Pair<TokenType, String>>) : FunctionParser<NumberNode, NumberNode>(
+        tokens
+    ) {
+        override fun consumeDelegate(pos: Int): ParseResult<NumberNode> {
+            val rep = ArgsParser(
+                tokens,
+                ExpressionParser(
+                    tokens, AlternativeParser(
+                        tokens, listOf(
+                            CalcFunctionParser(tokens),
+                            NumberParser(tokens),
+                        )
+                    )
+                ),
+                listOf()
+            ).consume(pos).nodeList[0].compute()
+            if (rep.name == "calc") {
+                return ParseResult(rep.posOffset, rep.argNodes)
+            } else {
+                throw ParseException("`calc` function expected but was `$rep.name`")
+            }
+        }
+    }
+
+    // HSL - 0.3turn?
+    class ColorFunctionParser(tokens: List<Pair<TokenType, String>>) :
+        FunctionParser<ColorNode, NumberNode>(tokens) {
+
+        override fun consumeDelegate(pos: Int): ParseResult<ColorNode> {
+            val possibleSeparatorPatterns = listOf(
+                listOf(
+                    SingleTokenParser(tokens, TokenType.COMMA_SEPARATOR),
+                    SingleTokenParser(tokens, TokenType.COMMA_SEPARATOR),
+                    SingleTokenParser(tokens, TokenType.COMMA_SEPARATOR),
+                ),
+                listOf(
+                    EmptyParser,
+                    EmptyParser,
+                    SingleTokenParser(tokens, TokenType.SLASH_SEPARATOR)
+                ),
+                listOf(
+                    SingleTokenParser(tokens, TokenType.COMMA_SEPARATOR),
+                    SingleTokenParser(tokens, TokenType.COMMA_SEPARATOR),
+                ),
+                listOf(
+                    EmptyParser,
+                    EmptyParser
+                ),
+            )
+            val funcArgParsers = possibleSeparatorPatterns.map {
+                ArgsParser(
+                    tokens,
+                    AlternativeParser(
+                        tokens, listOf(
+                            NumberParser(tokens),
+                            NoneParser(tokens),
+                            CalcFunctionParser(tokens),
+                        )
+                    ),
+                    it
+                )
+            }
+
+            val rep = AlternativeParser(
+                tokens,
+                funcArgParsers
+            ).consume(pos).nodeList[0].compute()
+
+            val func = ColorNode(rep.name, rep.argNodes)
+            return ParseResult(rep.posOffset, listOf(func))
+        }
+    }
+}
